88aeaeca356814e1bc0e427789ee5eb6e38fbdbc
lomogoo <yoshidarikuto@yahoo.com>
2025-07-04 19:22:47 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+// このファイルにAPIキーなどの秘密情報を記述します。
+// このファイルは .gitignore によってGitHubにはアップロードされません。
+
+const SUPABASE_CONFIG = {
+  URL: "https://impkzpdypusdminmyyea.supabase.co", // あなたのSupabase URL
+  ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltcGt6cGR5cHVzZG1pbm15eWVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1MTkxNTUsImV4cCI6MjA2NzA5NTE1NX0.soL4Acnn9sWNISYQ5ZhClKRTdZ-RCxLl3SZ3oYHu-Q0" // ステップ1で再生成したキー
+};
